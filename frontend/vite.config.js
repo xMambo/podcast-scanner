e21847e81cdc6e5062f9dbd5e328b2d88447e8ef
Mambo <44978347+xMambo@users.noreply.github.com>
2025-03-10 17:56:25 -0500

    fixing frontend errors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    jsxInject: `import React from 'react'`,
-    loader: 'jsx',
-    include: [
-      '**/*.js',
-      '**/*.jsx'
-    ]
-  }
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',  // Or just disable auto import
+      babel: {
+        plugins: [['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }]]
+      }
+    })
+  ]
 });
