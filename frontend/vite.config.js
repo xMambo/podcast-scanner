5b29dcbe3f8537296fb90e2713ab9123abeb2970
Mambo <44978347+xMambo@users.noreply.github.com>
2025-03-11 05:55:58 -0500

    clearing and reinstalling dependencies

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }]]
-      }
-    })
-  ]
+  plugins: [react()]
 });
