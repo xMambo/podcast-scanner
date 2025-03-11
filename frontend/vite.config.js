cf1f8a592396a7d15d8c496a0ab7059e47ed7469
Mambo <44978347+xMambo@users.noreply.github.com>
2025-03-11 05:34:02 -0500

    fixing vercel config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [
     react({
-      jsxImportSource: '@emotion/react',  // Or just disable auto import
       babel: {
         plugins: [['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }]]
       }
